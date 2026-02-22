54cd241bb5ab7cb72b19dab02895d029a9bc41a4
Fabian Mahdi <Fabian@Mahdi.at>
2026-02-22 17:54:00 +0100

    feat: added indentity to aspire

--- Aspire/AppHost.cs
+++ Aspire/AppHost.cs
@@ -1,3 +1,15 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume(isReadOnly: false);
+
+// identity
+var identityDb = postgres.AddDatabase("identityDb");
+
+builder.AddProject<Identity_WebHost>("identity")
+    .WithReference(identityDb)
+    .WaitFor(identityDb);
 
 builder.Build().Run();
