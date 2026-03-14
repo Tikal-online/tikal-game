0bcc5867ab7b52fe43fbe412daf4f9307a26aad3
Fabian Mahdi <Fabian@Mahdi.at>
2026-03-14 11:45:44 +0100

    open source test

--- Identity/Identity.IntegrationTests/Users/Register/RegisterTests.cs
+++ Identity/Identity.IntegrationTests/Users/Register/RegisterTests.cs
@@ -26,7 +26,6 @@ public class RegisterTests : IntegrationTestFixture
         var response = await Client.PostAsJsonAsync(url, registerDto);
 
         // then
-
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
     }
 
@@ -34,6 +33,7 @@ public class RegisterTests : IntegrationTestFixture
     public async Task GivenExistingUsername_WhenRegister_ThenReturnsConflict(RegisterDto registerDto)
     {
         // given
+
         await Client.PostAsJsonAsync(url, registerDto);
 
         // when
