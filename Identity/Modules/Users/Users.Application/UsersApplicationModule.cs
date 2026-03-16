94d396ea5dbb6cc6335f61146b1472d754e4f683
Fabian Mahdi <Fabian@Mahdi.at>
2026-03-16 22:34:03 +0100

    added missing dependencies

--- Identity/Modules/Users/Users.Application/UsersApplicationModule.cs
+++ Identity/Modules/Users/Users.Application/UsersApplicationModule.cs
@@ -1,6 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Users.Application.Configuration;
+using Users.Application.Services;
 
 namespace Users.Application;
 
@@ -11,6 +14,10 @@ public static class UsersApplicationModule
         public void AddUsersApplication(IConfiguration configuration)
         {
             services.Configure<TokenConfiguration>(configuration.GetSection(TokenConfiguration.Section));
+
+            services.AddSingleton<SecurityTokenHandler, JwtSecurityTokenHandler>();
+
+            services.AddScoped<TokenService, JwtTokenService>();
         }
     }
 }
