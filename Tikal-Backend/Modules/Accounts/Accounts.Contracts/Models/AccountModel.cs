5ba23e11ec3eb863398c3ecec17f1f12305f7699
Fabian Mahdi <Fabian@Mahdi.at>
2026-03-22 21:49:03 +0100

    added accounts.contracts

---
+++ Tikal-Backend/Modules/Accounts/Accounts.Contracts/Models/AccountModel.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Accounts.Contracts.Models;
+
+public sealed record AccountModel
+{
+    [Required] [MaxLength(100)] public required string UserId { get; set; }
+
+    [Required] [MaxLength(30)] public required string Name { get; set; }
+}
