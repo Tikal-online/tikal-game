d06eb417ac44c4b0fec7f3d3aac3a344ec964e9d
Fabian Mahdi <Fabian@Mahdi.at>
2026-03-24 22:11:26 +0100

    added tikal backend filter file

--- Tikal-Backend/API/RestApi.Controllers/Accounts/Dtos/AccountDto.cs
+++ Tikal-Backend/API/RestApi.Controllers/Accounts/Dtos/AccountDto.cs
@@ -4,7 +4,7 @@ namespace RestApi.Controllers.Accounts.Dtos;
 
 internal sealed record AccountDto
 {
-    [Required] [MaxLength(100)] public required string UserId { get; set; }
+    [Required][MaxLength(100)] public required string UserId { get; set; }
 
-    [Required] [MaxLength(30)] public required string Name { get; set; }
+    [Required][MaxLength(30)] public required string Name { get; set; }
 }
