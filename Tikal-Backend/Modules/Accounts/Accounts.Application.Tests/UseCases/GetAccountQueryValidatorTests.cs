bfc4e15ddb4ed1fdf99dc22b6b3fe50a56368326
Fabian Mahdi <Fabian@Mahdi.at>
2026-03-27 20:39:21 +0100

    feat: base structure for unit tests

---
+++ Tikal-Backend/Modules/Accounts/Accounts.Application.Tests/UseCases/GetAccountQueryValidatorTests.cs
@@ -0,0 +1,37 @@
+using Accounts.Application.UseCases.GetAccount;
+using Accounts.Contracts.Queries;
+using FluentValidation.TestHelper;
+
+namespace Accounts.Application.Tests.UseCases;
+
+internal sealed class GetAccountQueryValidatorTests
+{
+    // under test
+    private GetAccountQueryValidator validator;
+
+    [SetUp]
+    public void Setup()
+    {
+        validator = new GetAccountQueryValidator();
+    }
+
+    [TestCaseSource(typeof(GetAccountQueryTestCases), nameof(GetAccountQueryTestCases.ValidGetAccountQueries))]
+    public void GivenValidQuery_WhenValidate_ThenShouldNotHaveValidationErrors(GetAccountQuery query)
+    {
+        // when
+        var result = validator.TestValidate(query);
+
+        // then
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [TestCaseSource(typeof(GetAccountQueryTestCases), nameof(GetAccountQueryTestCases.InvalidGetAccountQueries))]
+    public void GivenInvalidQuery_WhenValidate_ThenShouldHaveValidationErrors(GetAccountQuery query)
+    {
+        // when
+        var result = validator.TestValidate(query);
+
+        // then
+        result.ShouldHaveValidationErrors();
+    }
+}
