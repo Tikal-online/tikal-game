c7d31dc26e6b9479d1636f81cb12f0f53c2e657f
Fabian Mahdi <Fabian@Mahdi.at>
2026-04-16 21:10:38 +0200

    fix: removed unnecessary cors config

--- Identity/Configuration/CorsConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace Identity.Configuration;
-
-internal sealed record CorsConfiguration
-{
-    public const string Position = "Cors";
-
-    public ICollection<string> AllowedOrigins { get; set; } = [];
-}
