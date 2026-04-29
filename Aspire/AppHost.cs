63af547e040fbd70e32f7cd72d49ebe54cc187de
Fabian Mahdi <Fabian@Mahdi.at>
2026-04-29 18:56:29 +0000

    feat updated devcontainer to run aspire

--- Aspire/AppHost.cs
+++ Aspire/AppHost.cs
@@ -2,7 +2,16 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+// parameters
+var clientSecret = builder.AddParameter("ClientSecret", true);
+
+var databasePassword = builder.AddParameter("DatabasePassword", true);
+
+var duendeLicense = builder.AddParameter("DuendeLicense", true);
+
+// database server
 var postgres = builder.AddPostgres("postgres")
+    .WithPassword(databasePassword)
     .WithDataVolume(isReadOnly: false)
     .WithPgAdmin(pgAdmin => pgAdmin
         .WithImageTag("latest")
@@ -13,6 +22,8 @@ var postgres = builder.AddPostgres("postgres")
 var identityDb = postgres.AddDatabase("identityDb");
 
 builder.AddProject<Identity>("identity")
+    .WithEnvironment("Client__Secret", clientSecret)
+    .WithEnvironment("Duende__LicenseKey", duendeLicense)
     .WithReference(identityDb)
     .WaitFor(identityDb);
 
@@ -27,6 +38,8 @@ builder.AddProject<TikalBackend_WebHost>("tikal-backend")
 var bffDb = postgres.AddDatabase("bffDb");
 
 var bff = builder.AddProject<BFF>("tikal-bff")
+    .WithEnvironment("Auth__Secret", clientSecret)
+    .WithEnvironment("Duende__LicenseKey", duendeLicense)
     .WithReference(bffDb)
     .WaitFor(bffDb);
 
