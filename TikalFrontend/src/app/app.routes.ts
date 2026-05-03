4ce700ab4fdd61de5658003c03e37b997489de00
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-05-03 16:07:20 +0200

    feat: added home screen (#214)

--- TikalFrontend/src/app/app.routes.ts
+++ TikalFrontend/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { Background } from './core/components/background/background';
+import { Home } from './core/components/home/home';
 
 export const routes: Routes = [
   {
     path: '',
     component: Background,
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+    ],
   },
 ];
