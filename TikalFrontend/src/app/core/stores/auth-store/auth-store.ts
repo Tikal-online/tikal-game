e215224d810b1cc6a4e4391ec523eb0dfc7b4e94
Fabian Mahdi <Fabian@Mahdi.at>
2026-05-10 11:56:01 +0000

    simplified authstore

--- TikalFrontend/src/app/core/stores/auth-store/auth-store.ts
+++ TikalFrontend/src/app/core/stores/auth-store/auth-store.ts
@@ -1,38 +1,29 @@
-import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
 import { AuthService, Session } from '../../services/auth-service/auth-service';
-import { inject } from '@angular/core';
+import { computed, inject } from '@angular/core';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { exhaustMap, pipe, tap } from 'rxjs';
 
-enum AuthStatus {
-  Authorized,
-  Unauthorized,
-}
-
 type AuthState = {
-  status: AuthStatus;
   session: Session | null;
 };
 
 const initalState: AuthState = {
-  status: AuthStatus.Unauthorized,
   session: null,
 };
 
 export const AuthStore = signalStore(
   withState(initalState),
+  withComputed(({ session }) => ({
+    isAuthenticated: computed(() => session() !== null),
+  })),
   withMethods((store, authService = inject(AuthService)) => ({
     loadSession: rxMethod<void>(
       pipe(
         exhaustMap(() => {
           return authService.GetSession().pipe(
             tap((result) => {
-              patchState(
-                store,
-                result.isOk()
-                  ? { status: AuthStatus.Authorized, session: result.value }
-                  : { status: AuthStatus.Unauthorized, session: null },
-              );
+              patchState(store, { session: result.isOk() ? result.value : null });
             }),
           );
         }),
