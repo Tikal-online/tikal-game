0adf403612a32530d66effad3760abce50fcdd2c
Fabian Mahdi <Fabian@Mahdi.at>
2026-05-12 08:16:22 +0000

    now uses dotnet dev certs

--- .devcontainer/init_devcontainer.sh
+++ .devcontainer/init_devcontainer.sh
@@ -6,7 +6,10 @@ echo "Setting ownership for .NET user secrets..."
 sudo chown -R vscode:vscode /home/vscode/.microsoft/usersecrets
 
 echo "Adding development CA certificate..."
-sudo cp /dev-certs/dotnet-dev-ca.pem /etc/ssl/certs/
+sudo cp /.aspnet/dev-certs/trust/dotnet-dev-ca.pem /etc/ssl/certs/
 
 echo "Updating certificate store..."
 sudo update-ca-certificates
+
+echo "Importing development certificate..."
+dotnet dev-certs https --import /.aspnet/dev-certs/trust/dotnet-dev-cert.pfx --clean -p secret
