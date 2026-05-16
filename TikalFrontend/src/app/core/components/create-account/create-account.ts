4b2c0681d7a9bfcaa469e4067e2edf323a6d2b46
Fabian Mahdi <Fabian@Mahdi.at>
2026-05-16 19:08:57 +0000

    added skeleton for create account screen

--- TikalFrontend/src/app/core/components/create-account/create-account.ts
+++ TikalFrontend/src/app/core/components/create-account/create-account.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Menu } from '../menu/menu';
+import { Button } from '../button/button';
+import { ButtonStyle } from '../button/button-type';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-create-account',
-  imports: [Menu],
+  imports: [Menu, Button],
   templateUrl: './create-account.html',
   styleUrl: './create-account.scss',
 })
-export class CreateAccount {}
+export class CreateAccount {
+  readonly ButtonStyle = ButtonStyle;
+}
