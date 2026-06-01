97244e0d66e7278811bf7e5aff816bc05f2e199c
Fabian Mahdi <Fabian@Mahdi.at>
2026-06-01 18:58:20 +0000

    added transloco base

--- TikalFrontend/src/app/app.config.ts
+++ TikalFrontend/src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { csrfHeaderInterceptor } from './core/interceptors/csrf-header.interceptor';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { appInitializer } from './app.initializer';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,16 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withViewTransitions()),
     provideHttpClient(withFetch(), withInterceptors([csrfHeaderInterceptor])),
     appInitializer,
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en'],
+        defaultLang: 'en',
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 };
--- TikalFrontend/src/app/core/components/home/home.ts
+++ TikalFrontend/src/app/core/components/home/home.ts
@@ -1,11 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { Button } from '../button/button';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-home',
-  imports: [NgOptimizedImage, Button],
+  imports: [NgOptimizedImage, Button, TranslocoDirective],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
