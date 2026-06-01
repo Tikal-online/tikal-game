90e4991bd8b156e9aa2e8807efb28b1de884ce57
Fabian Mahdi <Fabian@Mahdi.at>
2026-06-01 19:32:27 +0000

    http is now readonly

--- TikalFrontend/src/app/transloco-loader.ts
+++ TikalFrontend/src/app/transloco-loader.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
-  private http = inject(HttpClient);
+  private readonly http = inject(HttpClient);
 
   getTranslation(lang: string): Observable<Translation> {
     return this.http.get<Translation>(`/i18n/${lang}.json`);
