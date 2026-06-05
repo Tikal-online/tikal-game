31e5aa90830a0d9a0e82450d5414e6e6c4dcc42b
Fabian Mahdi <Fabian@Mahdi.at>
2026-06-05 10:13:17 +0000

    added integration tests

---
+++ TikalBackend/TikalBackend.IntegrationTests/Modules/Lobbies/GetPaginatedLobbiesTests.cs
@@ -0,0 +1,89 @@
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.WebUtilities;
+using RestApi.Controllers.Lobbies.Dtos;
+using TikalBackend.IntegrationTests.Extensions;
+
+namespace TikalBackend.IntegrationTests.Modules.Lobbies;
+
+internal sealed class GetPaginatedLobbiesTests : IntegrationTestFixture
+{
+    private const string lobbyUrl = "Lobbies";
+
+    [Test]
+    public async Task GivenUnauthenticatedUser_WhenGetPaginatedLobbies_ThenReturnsUnauthorized()
+    {
+        var queryParams = new Dictionary<string, string?>
+        {
+            ["pageSize"] = "10",
+            ["pageNumber"] = "1",
+            ["searchText"] = ""
+        };
+
+        var url = QueryHelpers.AddQueryString(lobbyUrl, queryParams);
+
+        // when
+        var response = await Client.GetAsync(url);
+
+        // then
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+    }
+
+    [Test]
+    public async Task GivenLobbiesAndSearchString_WhenGetPaginatedLobbies_ThenReturnsLobbiesWithMatchingNames()
+    {
+        // given
+        await CreateUserAccount(TestUser.Default);
+        await CreateUserAccount(TestUser.TestUser1);
+        await CreateUserAccount(TestUser.TestUser2);
+
+        await Client.PostAsyncWithUser(
+            lobbyUrl,
+            TestUser.Default,
+            new CreateLobbyDto
+            {
+                Name = "Lobby1",
+                MaxPlayers = 2
+            }
+        );
+
+        await Client.PostAsyncWithUser(
+            lobbyUrl,
+            TestUser.TestUser1,
+            new CreateLobbyDto
+            {
+                Name = "Lobby2",
+                MaxPlayers = 3
+            }
+        );
+
+        await Client.PostAsyncWithUser(
+            lobbyUrl,
+            TestUser.TestUser2,
+            new CreateLobbyDto
+            {
+                Name = "Lobby3",
+                MaxPlayers = 4
+            }
+        );
+
+        var queryParams = new Dictionary<string, string?>
+        {
+            ["pageSize"] = "10",
+            ["pageNumber"] = "1",
+            ["searchText"] = "2"
+        };
+
+        var url = QueryHelpers.AddQueryString(lobbyUrl, queryParams);
+
+        // when
+        var response = await Client.GetAsyncWithUser(url, TestUser.Default);
+
+        var lobbies = await response.Content.ReadFromJsonAsync<List<LobbySummaryDto>>();
+
+        // then
+        Assert.That(lobbies, Is.Not.Null);
+        Assert.That(lobbies, Has.Count.EqualTo(1));
+        Assert.That(lobbies.First().Name, Is.EqualTo("Lobby2"));
+    }
+}
--- TikalBackend/TikalBackend.IntegrationTests/TestUser.cs
+++ TikalBackend/TikalBackend.IntegrationTests/TestUser.cs
@@ -3,4 +3,8 @@ namespace TikalBackend.IntegrationTests;
 internal sealed record TestUser(string UserId, string Name)
 {
     public static readonly TestUser Default = new("7e2c296a-5af3-402e-9c80-6cc0a9548097", "TestUser");
+
+    public static readonly TestUser TestUser1 = new("92037a55-af69-4304-908f-267c3309e368", "TestUser1");
+
+    public static readonly TestUser TestUser2 = new("fff21445-42e9-420c-953e-5aa7208fc053", "TestUser2");
 }
