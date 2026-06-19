42ead4adbfdebf5cc0461ad398f0d5906ac500f7
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-06-19 21:44:08 +0200

    feat: scrapped existing frontend designs (#301)

--- TikalFrontend/src/app/core/components/home/home.ts
+++ TikalFrontend/src/app/core/components/home/home.ts
@@ -1,12 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
-import { Button } from '../button/button';
-import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-home',
-  imports: [NgOptimizedImage, Button, TranslocoDirective],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
