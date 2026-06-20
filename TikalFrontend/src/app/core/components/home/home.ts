3a0f91748afbfcb3659a68bf94682e202f59ae7e
Fabian Mahdi <fabian@mahdi.at>
2026-06-20 22:36:12 +0000

    added primary button base

--- TikalFrontend/src/app/core/components/home/home.ts
+++ TikalFrontend/src/app/core/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { LucideShovel } from '@lucide/angular';
 
 @Component({
   selector: 'app-home',
-  imports: [TranslocoDirective],
+  imports: [TranslocoDirective, LucideShovel],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
