a86ff9cb54931aed62c262d7ea00f6f4438579cb
Fabian Mahdi <fabian@mahdi.at>
2026-06-20 21:41:45 +0000

    landing page base

--- TikalFrontend/src/app/core/components/home/home.ts
+++ TikalFrontend/src/app/core/components/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TranslocoDirective],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- TikalFrontend/src/app/core/components/svg-background/svg-background.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-svg-background',
-  imports: [],
-  templateUrl: './svg-background.html',
-  styleUrl: './svg-background.scss',
-})
-export class SvgBackgound {}
