60cf3c0d2ba658c22508b0c813c05e48c3ae36cc
Fabian Mahdi <fabian@mahdi.at>
2026-06-21 20:40:39 +0000

    added basic navbar

--- TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LucideArrowLeft } from '@lucide/angular';
 
 @Component({
   selector: 'app-lobbies',
-  imports: [],
+  imports: [RouterLink, LucideArrowLeft],
   templateUrl: './lobbies.html',
   styleUrl: './lobbies.scss',
 })
