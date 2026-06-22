c4ab5c5a3d6ac5f116d87324375770aa2dc13845
Fabian Mahdi <fabian@mahdi.at>
2026-06-22 18:35:54 +0000

    added lobbies screen base layout

---
+++ TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-chat',
+  imports: [],
+  templateUrl: './global-chat.html',
+  styleUrl: './global-chat.scss',
+})
+export class GlobalChat {}
--- TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideArrowLeft } from '@lucide/angular';
+import { GlobalChat } from '../global-chat/global-chat';
 
 @Component({
   selector: 'app-lobbies',
-  imports: [RouterLink, LucideArrowLeft],
+  imports: [RouterLink, LucideArrowLeft, GlobalChat],
   templateUrl: './lobbies.html',
   styleUrl: './lobbies.scss',
 })
