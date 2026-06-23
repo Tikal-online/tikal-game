c02b8e53c4454e57b9b87f599d4ea89b3091a8d7
Fabian Mahdi <fabian@mahdi.at>
2026-06-23 21:32:11 +0000

    added global chat messages

--- TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat.ts
+++ TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { LucideMessageSquare, LucideSendHorizontal, LucideX } from '@lucide/angular';
+import { GlobalChatMessage } from '../global-chat-message/global-chat-message';
 
 @Component({
   selector: 'app-global-chat',
-  imports: [LucideX, LucideMessageSquare, LucideSendHorizontal],
+  imports: [LucideX, LucideMessageSquare, LucideSendHorizontal, GlobalChatMessage],
   templateUrl: './global-chat.html',
   styleUrl: './global-chat.scss',
 })
