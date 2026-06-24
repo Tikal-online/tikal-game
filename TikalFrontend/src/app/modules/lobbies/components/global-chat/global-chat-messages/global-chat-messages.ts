6535f94d450a0904fcd87c79876925614adc5814
Fabian Mahdi <fabian@mahdi.at>
2026-06-24 17:00:57 +0000

    moved chat components to where they are used

--- TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat-messages/global-chat-messages.ts
+++ TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat-messages/global-chat-messages.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { GlobalChatStore } from '../../../stores/global-chat/global-chat-store';
-import { MyChatMessage } from '../my-chat-message/my-chat-message';
-import { EnemyChatMessage } from '../enemy-chat-message/enemy-chat-message';
+import { MyChatMessage } from './my-chat-message/my-chat-message';
+import { EnemyChatMessage } from './enemy-chat-message/enemy-chat-message';
 import { ChatMessage } from '../../../services/global-chat/global-chat-service';
 import { AuthStore } from '../../../../../core/stores/auth-store/auth-store';
 
