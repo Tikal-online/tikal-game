3eb8f619c7f582deb50c9edd5872eeb91163dcbf
Fabian Mahdi <fabian@mahdi.at>
2026-06-24 13:52:17 +0000

    added user id to chat message

--- TikalBackend/API/SignalRApi.Hubs/Lobbies/Dtos/ChatMessageDto.cs
+++ TikalBackend/API/SignalRApi.Hubs/Lobbies/Dtos/ChatMessageDto.cs
@@ -5,6 +5,10 @@ namespace SignalRApi.Hubs.Lobbies.Dtos;
 public record ChatMessageDto
 {
     [Required]
+    [MaxLength(100)]
+    public required string UserId { get; set; }
+
+    [Required]
     [MaxLength(30)]
     public required string Username { get; set; }
 
