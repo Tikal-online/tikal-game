b0d545857231c2dade27453cc5994835e9a1c2bc
Fabian Mahdi <fabian@mahdi.at>
2026-06-25 13:17:23 +0000

    downgraded scalar package since it breaks

---
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies-list/lobbies-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lobbies-list',
+  imports: [],
+  templateUrl: './lobbies-list.html',
+  styleUrl: './lobbies-list.scss',
+})
+export class LobbiesList {}
