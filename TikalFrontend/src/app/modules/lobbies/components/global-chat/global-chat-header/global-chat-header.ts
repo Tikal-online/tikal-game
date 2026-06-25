1dc8d20f4d37b4a8b2c95b21f8c52c90563e450d
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-06-25 14:26:00 +0200

    feat: global chat connection status is now displayed in the global chat header (#324)

--- TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat-header/global-chat-header.ts
+++ TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat-header/global-chat-header.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
-import { LucideX } from '@lucide/angular';
+import { LucideWifi, LucideWifiOff, LucideX } from '@lucide/angular';
 import { GlobalChatStore } from '../../../stores/global-chat/global-chat-store';
 
 @Component({
   selector: 'app-global-chat-header',
-  imports: [TranslocoDirective, LucideX],
+  imports: [TranslocoDirective, LucideX, LucideWifi, LucideWifiOff],
   templateUrl: './global-chat-header.html',
   styleUrl: './global-chat-header.scss',
 })
