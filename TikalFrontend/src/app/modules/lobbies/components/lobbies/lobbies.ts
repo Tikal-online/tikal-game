388e8b93c503ad9e8e86170ba62e5d76b075fbf1
Fabian Mahdi <fabian@mahdi.at>
2026-06-26 16:50:54 +0000

    lobbies-list base

--- TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideArrowLeft } from '@lucide/angular';
 import { GlobalChat } from '../global-chat/global-chat';
+import { LobbiesList } from '../lobbies-list/lobbies-list';
 
 @Component({
   selector: 'app-lobbies',
-  imports: [RouterLink, LucideArrowLeft, GlobalChat],
+  imports: [RouterLink, LucideArrowLeft, GlobalChat, LobbiesList],
   templateUrl: './lobbies.html',
   styleUrl: './lobbies.scss',
 })
