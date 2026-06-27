7c10fad4d98069ed0a07ec5c46ce3f31e9dd9747
Fabian Mahdi <fabian@mahdi.at>
2026-06-27 13:14:41 +0000

    extracted header into separate component

---
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies-list/lobbies-list-header/lobbies-list-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lobbies-list-header',
+  imports: [],
+  templateUrl: './lobbies-list-header.html',
+  styleUrl: './lobbies-list-header.scss',
+})
+export class LobbiesListHeader {}
--- TikalFrontend/src/app/modules/lobbies/components/lobbies-list/lobbies-list.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies-list/lobbies-list.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { LucideGlobe, LucideUsers } from '@lucide/angular';
 import { LobbySummaryStore } from '../../stores/lobby/lobby-summary-store';
 import { LobbiesListPagination } from './lobbies-list-pagination/lobbies-list-pagination';
+import { LobbiesListHeader } from './lobbies-list-header/lobbies-list-header';
 
 @Component({
   selector: 'app-lobbies-list',
-  imports: [LucideGlobe, LucideUsers, LobbiesListPagination],
+  imports: [LucideGlobe, LucideUsers, LobbiesListPagination, LobbiesListHeader],
   templateUrl: './lobbies-list.html',
   styleUrl: './lobbies-list.scss',
 })
