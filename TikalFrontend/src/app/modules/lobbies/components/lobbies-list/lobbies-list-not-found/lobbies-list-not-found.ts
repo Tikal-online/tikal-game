fdf28cfa28b7752826ff400d104b95022e123660
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-06-27 21:38:50 +0200

    chore: added missing translations for lobby list components (#339)

--- TikalFrontend/src/app/modules/lobbies/components/lobbies-list/lobbies-list-not-found/lobbies-list-not-found.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies-list/lobbies-list-not-found/lobbies-list-not-found.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
 import { LucideSearch } from '@lucide/angular';
 
 @Component({
   selector: 'app-lobbies-list-not-found',
-  imports: [LucideSearch],
+  imports: [LucideSearch, TranslocoDirective],
   templateUrl: './lobbies-list-not-found.html',
   styleUrl: './lobbies-list-not-found.scss',
 })
