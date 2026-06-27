53b99b5aee52d30998e03d55f1f62523a64f4072
Fabian Mahdi <fabian@mahdi.at>
2026-06-27 19:24:40 +0000

    added header translations

--- TikalFrontend/src/app/modules/lobbies/components/lobbies-list/lobbies-list-header/lobbies-list-header.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies-list/lobbies-list-header/lobbies-list-header.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { LucideRefreshCw } from '@lucide/angular';
 import { LobbySummaryStore } from '../../../stores/lobby/lobby-summary-store';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-lobbies-list-header',
-  imports: [LucideRefreshCw],
+  imports: [LucideRefreshCw, TranslocoDirective],
   templateUrl: './lobbies-list-header.html',
   styleUrl: './lobbies-list-header.scss',
 })
