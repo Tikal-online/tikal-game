8fb3bdbe753f1a5653673a59e2e27ab1a54836f5
Fabian Mahdi <fabian@mahdi.at>
2026-07-01 17:28:09 +0000

    base

--- TikalFrontend/src/app/modules/lobbies/components/create-lobby/create-lobby.ts
+++ TikalFrontend/src/app/modules/lobbies/components/create-lobby/create-lobby.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { form, FormField, FormRoot, max, maxLength, min, required } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { LucideArrowLeft } from '@lucide/angular';
+import { AccountStore } from '../../../../core/stores/account-store/account-store';
+
+type LobbyData = {
+  name: string;
+  maxPlayers: number;
+};
 
 @Component({
   selector: 'app-create-lobby',
-  imports: [LucideArrowLeft, RouterLink],
+  imports: [LucideArrowLeft, RouterLink, FormRoot, FormField],
   templateUrl: './create-lobby.html',
   styleUrl: './create-lobby.scss',
 })
-export class CreateLobby {}
+export class CreateLobby {
+  private readonly accountStore = inject(AccountStore);
+
+  private readonly lobbyData = signal<LobbyData>({
+    name: `${this.accountStore.account()?.name}s Lobby`,
+    maxPlayers: 4,
+  });
+
+  readonly lobbyForm = form(this.lobbyData, (schemaPath) => {
+    required(schemaPath.name);
+    maxLength(schemaPath.name, 30);
+    required(schemaPath.maxPlayers);
+    min(schemaPath.maxPlayers, 2);
+    max(schemaPath.maxPlayers, 4);
+  });
+}
