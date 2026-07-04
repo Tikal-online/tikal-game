98801dce1e8a197496aa7b0596bf2bddbb8530fd
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-07-04 08:44:31 +0200

    feat: added max player form selection (#378)

---
+++ TikalFrontend/src/app/modules/lobbies/components/create-lobby/max-players-selection/max-players-selection.ts
@@ -0,0 +1,18 @@
+import { Component, input, output } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
+
+@Component({
+  selector: 'app-max-players-selection',
+  imports: [TranslocoDirective],
+  templateUrl: './max-players-selection.html',
+  styleUrl: './max-players-selection.scss',
+})
+export class MaxPlayersSelection {
+  readonly maxPlayers = input.required<number>();
+
+  readonly maxPlayersSet = output<number>();
+
+  setMaxPlayers(maxPlayers: number): void {
+    this.maxPlayersSet.emit(maxPlayers);
+  }
+}
