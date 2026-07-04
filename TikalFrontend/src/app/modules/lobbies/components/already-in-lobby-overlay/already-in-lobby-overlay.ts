013769376e7f1a1b3c49fb898b7bc02e996bd5ee
Fabian Mahdi <fabian@mahdi.at>
2026-07-04 19:30:54 +0000

    added danger icon

--- TikalFrontend/src/app/modules/lobbies/components/already-in-lobby-overlay/already-in-lobby-overlay.ts
+++ TikalFrontend/src/app/modules/lobbies/components/already-in-lobby-overlay/already-in-lobby-overlay.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { LucideTriangleAlert } from '@lucide/angular';
 
 @Component({
   selector: 'app-already-in-lobby-overlay',
-  imports: [TranslocoDirective],
+  imports: [TranslocoDirective, LucideTriangleAlert],
   templateUrl: './already-in-lobby-overlay.html',
   styleUrl: './already-in-lobby-overlay.scss',
 })
