1a662e9010c0692d4f12275ee5fee353604c4adb
Fabian Mahdi <fabian@mahdi.at>
2026-07-04 09:33:59 +0000

    form can now be submitted

--- TikalFrontend/src/app/modules/lobbies/components/create-lobby/create-lobby.ts
+++ TikalFrontend/src/app/modules/lobbies/components/create-lobby/create-lobby.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { form, FormField, FormRoot, max, maxLength, min, required } from '@angular/forms/signals';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { LucideArrowLeft } from '@lucide/angular';
 import { AccountStore } from '../../../../core/stores/account-store/account-store';
 import { LobbyPlayerList } from '../lobby-player-list/lobby-player-list';
@@ -8,6 +8,8 @@ import { Player } from '../../models/player';
 import { LobbyPlayerListHeader } from '../lobby-player-list-header/lobby-player-list-header';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { MaxPlayersSelection } from './max-players-selection/max-players-selection';
+import { LobbyService } from '../../services/lobby/lobby-service';
+import { firstValueFrom } from 'rxjs';
 
 type LobbyData = {
   name: string;
@@ -32,6 +34,10 @@ type LobbyData = {
 export class CreateLobby {
   private readonly accountStore = inject(AccountStore);
 
+  private readonly lobbyService = inject(LobbyService);
+
+  private readonly router = inject(Router);
+
   readonly defaultPlayer = signal<Player>({
     userId: this.accountStore.account()!.userId,
     name: this.accountStore.account()!.name,
@@ -44,13 +50,36 @@ export class CreateLobby {
     maxPlayers: 4,
   });
 
-  readonly lobbyForm = form(this.lobbyData, (schemaPath) => {
-    required(schemaPath.name);
-    maxLength(schemaPath.name, 30);
-    required(schemaPath.maxPlayers);
-    min(schemaPath.maxPlayers, 2);
-    max(schemaPath.maxPlayers, 4);
-  });
+  readonly lobbyForm = form(
+    this.lobbyData,
+    (schemaPath) => {
+      required(schemaPath.name);
+      maxLength(schemaPath.name, 30);
+      required(schemaPath.maxPlayers);
+      min(schemaPath.maxPlayers, 2);
+      max(schemaPath.maxPlayers, 4);
+    },
+    {
+      submission: {
+        action: async (field) => {
+          const data = field().value();
+
+          const result = await firstValueFrom(
+            this.lobbyService.createLobby(data.name, data.maxPlayers),
+          );
+
+          if (result.isOk()) {
+            this.router.navigate(['/lobbies']);
+            return;
+          }
+
+          return {
+            kind: 'serverError',
+          };
+        },
+      },
+    },
+  );
 
   setMaxPlayers(maxPlayers: number): void {
     this.lobbyData.update((data) => ({ ...data, maxPlayers: maxPlayers }));
