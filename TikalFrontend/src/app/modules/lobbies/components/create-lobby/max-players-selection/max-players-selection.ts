b141d65544516ceee1b5f6af8941ed68d864b792
Fabian Mahdi <fabian@mahdi.at>
2026-07-04 06:30:27 +0000

    control work now

--- TikalFrontend/src/app/modules/lobbies/components/create-lobby/max-players-selection/max-players-selection.ts
+++ TikalFrontend/src/app/modules/lobbies/components/create-lobby/max-players-selection/max-players-selection.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-max-players-selection',
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './max-players-selection.html',
   styleUrl: './max-players-selection.scss',
 })
-export class MaxPlayersSelection {}
+export class MaxPlayersSelection {
+  readonly maxPlayers = input.required<number>();
+
+  readonly maxPlayersSet = output<number>();
+
+  setMaxPlayers(maxPlayers: number): void {
+    this.maxPlayersSet.emit(maxPlayers);
+  }
+}
