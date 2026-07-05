b2b0e822f8bbb6e7d767d5e13745ce9767a359af
Fabian Mahdi <fabian@mahdi.at>
2026-07-05 06:45:58 +0000

    minor fixes

--- TikalFrontend/src/app/modules/lobbies/components/already-in-lobby-overlay/already-in-lobby-overlay.ts
+++ TikalFrontend/src/app/modules/lobbies/components/already-in-lobby-overlay/already-in-lobby-overlay.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { LucideTriangleAlert } from '@lucide/angular';
 
 @Component({
   selector: 'app-already-in-lobby-overlay',
-  imports: [TranslocoDirective, LucideTriangleAlert],
+  imports: [TranslocoDirective, LucideTriangleAlert, RouterLink],
   templateUrl: './already-in-lobby-overlay.html',
   styleUrl: './already-in-lobby-overlay.scss',
 })
