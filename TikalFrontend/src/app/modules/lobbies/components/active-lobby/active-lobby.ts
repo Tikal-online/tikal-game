79b5a9f9e0ab0c8f0a0b73ccdec1d10ba58c573f
Fabian Mahdi <fabian@mahdi.at>
2026-07-05 06:11:46 +0000

    added initial active lobby screen

--- TikalFrontend/src/app/modules/lobbies/components/active-lobby/active-lobby.ts
+++ TikalFrontend/src/app/modules/lobbies/components/active-lobby/active-lobby.ts
@@ -1,12 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { ActiveLobbyStore } from '../../stores/lobby/active-lobby-store';
+import { LobbyPlayerListHeader } from '../lobby-player-list-header/lobby-player-list-header';
+import { LobbyPlayerList } from '../lobby-player-list/lobby-player-list';
 
 @Component({
   selector: 'app-active-lobby',
-  imports: [],
+  imports: [LobbyPlayerListHeader, LobbyPlayerList],
   templateUrl: './active-lobby.html',
   styleUrl: './active-lobby.scss',
 })
 export class ActiveLobby {
   readonly activeLobbyStore = inject(ActiveLobbyStore);
+
+  constructor() {
+    this.activeLobbyStore.loadActiveLobby();
+  }
 }
