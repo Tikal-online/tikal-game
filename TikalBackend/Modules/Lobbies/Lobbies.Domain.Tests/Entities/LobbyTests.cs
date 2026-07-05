55597c6d13ffa2ab5fe548b1af9c0e275412fa18
Fabian Mahdi <fabian@mahdi.at>
2026-07-05 18:04:50 +0000

    added unit tests for lobby

---
+++ TikalBackend/Modules/Lobbies/Lobbies.Domain.Tests/Entities/LobbyTests.cs
@@ -0,0 +1,40 @@
+using Lobbies.Domain.Entities;
+using Lobbies.Domain.Tests.Data;
+
+namespace Lobbies.Domain.Tests.Entities;
+
+internal sealed class LobbyTests
+{
+    // test data
+    public static IEnumerable<Lobby> LobbyWithMultiplePlayersAndOneOwnerTestCases =>
+        LobbyTestCases.ValidLobbyTestCases.Where(l => l.Players.Count > 1 && l.Players.Count(p => p.IsOwner) == 1);
+
+    [TestCaseSource(typeof(LobbyTestCases), nameof(LobbyTestCases.ValidLobbyTestCases))]
+    public void GivenLobby_WhenRemovePlayer_ThenRemovesPlayerFromList(Lobby lobby)
+    {
+        // given
+        var playerToRemove = lobby.Players.First();
+
+        // when
+        lobby.RemovePlayer(playerToRemove);
+
+        // then
+        Assert.That(lobby.Players, Does.Not.Contain(playerToRemove));
+    }
+
+    [TestCaseSource(nameof(LobbyWithMultiplePlayersAndOneOwnerTestCases))]
+    public void GivenLobbyWithMultiplePlayersAndOneOwner_WhenOwnerIsRemoved_ThenPromotesAnotherPlayerToOwner(
+        Lobby lobby
+    )
+    {
+        // given
+        var playerToRemove = lobby.Players.First(p => p.IsOwner);
+
+        // when
+        lobby.RemovePlayer(playerToRemove);
+
+        // then
+        Assert.That(lobby.Players, Does.Not.Contain(playerToRemove));
+        Assert.That(lobby.Players.Any(p => p.IsOwner), Is.True);
+    }
+}
