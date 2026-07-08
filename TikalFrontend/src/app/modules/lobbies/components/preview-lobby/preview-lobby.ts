06cc0043e2f0821fa752d5627e373af78424dcfe
Fabian Mahdi <fabian@mahdi.at>
2026-07-08 18:39:10 +0000

    added preview screen

--- TikalFrontend/src/app/modules/lobbies/components/preview-lobby/preview-lobby.ts
+++ TikalFrontend/src/app/modules/lobbies/components/preview-lobby/preview-lobby.ts
@@ -1,12 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { PreviewLobbyStore } from '../../stores/lobby/preview-lobby-store';
+import { LucideArrowLeft } from '@lucide/angular';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { LobbyPlayerListHeader } from '../lobby-player-list-header/lobby-player-list-header';
+import { LobbyPlayerList } from '../lobby-player-list/lobby-player-list';
 
 @Component({
   selector: 'app-preview-lobby',
-  imports: [],
+  imports: [LucideArrowLeft, RouterLink, LobbyPlayerListHeader, LobbyPlayerList],
   templateUrl: './preview-lobby.html',
   styleUrl: './preview-lobby.scss',
 })
 export class PreviewLobby {
+  private readonly route = inject(ActivatedRoute);
+
   readonly previewLobbyStore = inject(PreviewLobbyStore);
+
+  constructor() {
+    const lobbyId = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.previewLobbyStore.loadLobby(lobbyId);
+  }
 }
