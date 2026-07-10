a82003af5d18dbdbd6c02ac47b47ab3274af43ca
Fabian Mahdi <fabian@mahdi.at>
2026-07-10 20:58:53 +0000

    minor fixes

--- TikalFrontend/src/app/modules/lobbies/components/active-lobby/active-lobby.ts
+++ TikalFrontend/src/app/modules/lobbies/components/active-lobby/active-lobby.ts
@@ -2,7 +2,6 @@ import { Component, DestroyRef, inject } from '@angular/core';
 import { ActiveLobbyStore } from '../../stores/lobby/active-lobby-store';
 import { LobbyPlayerListHeader } from '../lobby-player-list-header/lobby-player-list-header';
 import { LobbyPlayerList } from '../lobby-player-list/lobby-player-list';
-import { Router } from '@angular/router';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { LobbyNotFoundOverlay } from '../error-overlays/lobby-not-found-overlay/lobby-not-found-overlay';
 import { LucideLoaderCircle } from '@lucide/angular';
@@ -20,8 +19,6 @@ import { LucideLoaderCircle } from '@lucide/angular';
   styleUrl: './active-lobby.scss',
 })
 export class ActiveLobby {
-  private readonly router = inject(Router);
-
   readonly activeLobbyStore = inject(ActiveLobbyStore);
 
   constructor() {
