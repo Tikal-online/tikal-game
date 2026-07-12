d7197ce0562a9289b865f3969ee85be099cff9cf
Fabian Mahdi <fabian@mahdi.at>
2026-07-12 13:08:41 +0000

    made chat message component reuseable

--- TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat.ts
+++ TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat.ts
@@ -1,23 +1,23 @@
 import { Component, DestroyRef, inject } from '@angular/core';
 import { LucideMessageSquare } from '@lucide/angular';
 import { GlobalChatStore } from '../../stores/global-chat/global-chat-store';
-import { GlobalChatMessages } from './global-chat-messages/global-chat-messages';
 import { GlobalChatForm } from './global-chat-form/global-chat-form';
 import { GlobalChatDisconnected } from './global-chat-disconnected/global-chat-disconnected';
 import { GlobalChatConnecting } from './global-chat-connecting/global-chat-connecting';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { ChatHeader } from '../chat-header/chat-header';
+import { ChatMessages } from '../chat-messages/chat-messages';
 
 @Component({
   selector: 'app-global-chat',
   imports: [
     TranslocoDirective,
     LucideMessageSquare,
-    GlobalChatMessages,
     GlobalChatForm,
     GlobalChatDisconnected,
     GlobalChatConnecting,
     ChatHeader,
+    ChatMessages,
   ],
   templateUrl: './global-chat.html',
   styleUrl: './global-chat.scss',
