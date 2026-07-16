1aad43be18270bebe555eb50fd7b928b86e3a1a7
Fabian Mahdi <fabian@mahdi.at>
2026-07-16 21:22:06 +0000

    made chat disconnected component reuseable

--- TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat.ts
+++ TikalFrontend/src/app/modules/lobbies/components/global-chat/global-chat.ts
@@ -1,23 +1,23 @@
 import { Component, DestroyRef, inject } from '@angular/core';
 import { LucideMessageSquare } from '@lucide/angular';
 import { GlobalChatStore } from '../../stores/global-chat/global-chat-store';
-import { GlobalChatDisconnected } from './global-chat-disconnected/global-chat-disconnected';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { ChatHeader } from '../chat-header/chat-header';
 import { ChatMessages } from '../chat-messages/chat-messages';
 import { ChatForm } from '../chat-form/chat-form';
 import { ChatConnecting } from '../chat-connecting/chat-connecting';
+import { ChatDisconnected } from '../chat-disconnected/chat-disconnected';
 
 @Component({
   selector: 'app-global-chat',
   imports: [
     TranslocoDirective,
     LucideMessageSquare,
-    GlobalChatDisconnected,
     ChatHeader,
     ChatMessages,
     ChatForm,
     ChatConnecting,
+    ChatDisconnected,
   ],
   templateUrl: './global-chat.html',
   styleUrl: './global-chat.scss',
