83e09829ce362f7bec78fcfb37392ad08299eb0a
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-08-01 12:15:33 +0200

    fix: calls hitting the bff catch all endpoint now have display name of the root span set to the called endpoint instead of a generic description (#465)

--- BFF/Program.cs
+++ BFF/Program.cs
@@ -128,7 +128,21 @@ builder.Services.AddOpenTelemetry()
     {
         tracing
             .AddSource(builder.Environment.ApplicationName)
-            .AddAspNetCoreInstrumentation()
+            .AddAspNetCoreInstrumentation(options =>
+            {
+                // set the display name for calls hitting the catch-all forwarding endpoint to match the called endpoint
+                // instead of a generic catch-all name
+                options.EnrichWithHttpResponse = (activity, httpResponse) =>
+                {
+                    var httpRequest = httpResponse.HttpContext.Request;
+
+                    if (httpRequest.Path.StartsWithSegments("/Api"))
+                    {
+                        activity.DisplayName = $"{httpRequest.Method} {httpRequest.Path}";
+                        activity.SetTag("http.route", httpRequest.Path.Value);
+                    }
+                };
+            })
             .AddHttpClientInstrumentation()
             .AddNpgsql()
             .AddOtlpExporter();
