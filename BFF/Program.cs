4f0f9f0d5d81f38519fcd23e0cbecf3b27a5df0f
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-08-08 17:46:28 +0200

    fix: removed temporary PII log information (#481)

--- BFF/Program.cs
+++ BFF/Program.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Logging;
 using Npgsql;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
@@ -20,8 +19,6 @@ using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
-
-IdentityModelEventSource.ShowPII = true;
 
 builder.Services.AddControllers();
 
