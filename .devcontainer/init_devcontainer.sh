3e7a1c87e1bc804efe1202732910b3b20f30a1d9
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-08-09 07:45:14 +0200

    fix: required dotnet version is now installed automatically when building dev container (#482)

--- .devcontainer/init_devcontainer.sh
+++ .devcontainer/init_devcontainer.sh
@@ -11,5 +11,11 @@ sudo cp /.aspnet/dev-certs/trust/dotnet-dev-ca.pem /etc/ssl/certs/
 echo "Updating certificate store..."
 sudo update-ca-certificates
 
+echo "Installing required dotnet sdk version"
+curl -sSL https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh --jsonfile global.json
+rm dotnet-install.sh
+
 echo "Importing development certificate..."
 dotnet dev-certs https --import /.aspnet/dev-certs/trust/dotnet-dev-cert.pfx --clean -p secret
