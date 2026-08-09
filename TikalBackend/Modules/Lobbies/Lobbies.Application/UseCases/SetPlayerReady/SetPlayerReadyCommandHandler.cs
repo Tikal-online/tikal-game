b82edcae7d838e13e04e68e36968ec5ccf5c0bc0
Fabian Mahdi <fabian@mahdi.at>
2026-08-09 14:26:11 +0000

    added set player ready command

---
+++ TikalBackend/Modules/Lobbies/Lobbies.Application/UseCases/SetPlayerReady/SetPlayerReadyCommandHandler.cs
@@ -0,0 +1,49 @@
+using Lobbies.Application.DataAccess;
+using Lobbies.Contracts.Commands;
+using Lobbies.Contracts.Errors;
+using OneOf;
+using OneOf.Types;
+using Shared.Application.Contexts;
+using Shared.Contracts.Messaging;
+
+namespace Lobbies.Application.UseCases.SetPlayerReady;
+
+internal sealed class SetPlayerReadyCommandHandler
+    : CommandHandler<SetPlayerReadyCommand, OneOf<Success, PlayerNotInALobby>>
+{
+    private readonly PlayerRepository playerRepository;
+
+    private readonly UnitOfWork unitOfWork;
+
+    private readonly AccountContext accountContext;
+
+    public SetPlayerReadyCommandHandler(
+        PlayerRepository playerRepository,
+        UnitOfWork unitOfWork,
+        AccountContext accountContext
+    )
+    {
+        this.playerRepository = playerRepository;
+        this.unitOfWork = unitOfWork;
+        this.accountContext = accountContext;
+    }
+
+    public async Task<OneOf<Success, PlayerNotInALobby>> Handle(
+        SetPlayerReadyCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        var player = await playerRepository.GetByUserIdAsync(accountContext.Account.UserId);
+
+        if (player is null)
+        {
+            return new PlayerNotInALobby();
+        }
+
+        player.ReadyUp();
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return new Success();
+    }
+}
