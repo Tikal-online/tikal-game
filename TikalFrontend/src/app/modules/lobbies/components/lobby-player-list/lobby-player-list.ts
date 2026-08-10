2e1b792b0b83006fd24297f9525b071881aff819
Fabian Mahdi <fabian@mahdi.at>
2026-08-10 21:23:16 +0000

    simplified lobby list

--- TikalFrontend/src/app/modules/lobbies/components/lobby-player-list/lobby-player-list.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobby-player-list/lobby-player-list.ts
@@ -1,6 +1,5 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Player } from '../../models/player';
-import { AccountStore } from '../../../../core/stores/account-store/account-store';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { LucideCrown } from '@lucide/angular';
 
@@ -11,15 +10,9 @@ import { LucideCrown } from '@lucide/angular';
   styleUrl: './lobby-player-list.scss',
 })
 export class LobbyPlayerList {
-  readonly accountStore = inject(AccountStore);
-
   readonly maxPlayers = input.required<number>();
 
   readonly players = input.required<Player[]>();
 
   readonly isLoading = input<boolean>(false);
-
-  isMe(player: Player): boolean {
-    return player.userId === this.accountStore.account()?.userId;
-  }
 }
