9a32bc814f278005ecb35cbb7a8fb48fca055bcc
Fabian Mahdi <fabian@mahdi.at>
2026-08-16 13:23:56 +0000

    removed unused game repo method

--- TikalBackend/Modules/Games/Games.Infrastructure/Repositories/DbGameRepository.cs
+++ TikalBackend/Modules/Games/Games.Infrastructure/Repositories/DbGameRepository.cs
@@ -1,7 +1,6 @@
 using Games.Application.DataAccess;
 using Games.Domain.Entities;
 using Games.Infrastructure.Database;
-using Microsoft.EntityFrameworkCore;
 
 namespace Games.Infrastructure.Repositories;
 
@@ -17,16 +16,5 @@ internal sealed class DbGameRepository : GameRepository
     public void Create(Game game)
     {
         gamesDbContext.Add(game);
-    }
-
-    public Task<Game?> GetByUserId(string userId)
-    {
-        return gamesDbContext.Games
-            .Include(game => game.Players)
-            .Include(game => game.TileMap)
-            .ThenInclude(tileMap => tileMap.Tiles)
-            .ThenInclude(tile => tile.TroopAssignments)
-            .AsSplitQuery()
-            .SingleOrDefaultAsync(g => g.Players.Any(p => p.UserId == userId));
     }
 }
