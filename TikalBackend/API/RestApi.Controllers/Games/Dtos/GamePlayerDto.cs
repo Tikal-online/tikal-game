589297dd1d6621a9638ce6820c0e677d17ba3110
Fabian Mahdi <fabian@mahdi.at>
2026-08-16 15:25:33 +0000

    enum is now serialized as string

--- TikalBackend/API/RestApi.Controllers/Games/Dtos/GamePlayerDto.cs
+++ TikalBackend/API/RestApi.Controllers/Games/Dtos/GamePlayerDto.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using RestApi.Controllers.Shared.Dtos.Enums;
 
 namespace RestApi.Controllers.Games.Dtos;
@@ -13,6 +14,7 @@ public sealed record GamePlayerDto
     [MaxLength(30)]
     public required string Name { get; set; }
 
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public required ColourDto Colour { get; set; }
 
     public required int Points { get; set; }
