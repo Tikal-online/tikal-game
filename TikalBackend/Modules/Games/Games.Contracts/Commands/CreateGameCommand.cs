0e35dc82353ecadc10037f1f90746c0a6366397b
Fabian Mahdi <fabian@mahdi.at>
2026-08-16 06:58:51 +0000

    game creation is now a result of a notification

--- TikalBackend/Modules/Games/Games.Contracts/Commands/CreateGameCommand.cs
+++
@@ -1,9 +0,0 @@
-using OneOf;
-using OneOf.Types;
-using Shared.Contracts.Enums;
-using Shared.Contracts.Messaging;
-
-namespace Games.Contracts.Commands;
-
-public sealed record CreateGameCommand(long LobbyId, IReadOnlyDictionary<ColourModel, string> Players)
-    : Command<OneOf<Success>>;
