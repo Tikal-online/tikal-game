67dd6ab3ea74ac77a9882f3904cda6d05adb5ab7
Fabian Mahdi <fabian@mahdi.at>
2026-08-17 20:24:27 +0000

    added migration

--- TikalBackend/Modules/Games/Games.Infrastructure/Database/Migrations/20260817202309_Remove_Tilemap.Designer.cs
+++ TikalBackend/Modules/Games/Games.Infrastructure/Database/Migrations/20260817202309_Remove_Tilemap.Designer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Games.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Games.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(GamesDbContext))]
-    partial class GamesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260817202309_Remove_Tilemap")]
+    partial class Remove_Tilemap
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
