904e4fc132c51bf03e960a91888be9b200b8aaaf
Fabian Mahdi <fabian@mahdi.at>
2026-08-22 10:04:05 +0000

    disabled onboarding

--- TikalComponents/projects/tikal-ui-components/.storybook/main.ts
+++ TikalComponents/projects/tikal-ui-components/.storybook/main.ts
@@ -3,6 +3,9 @@ import type { StorybookConfig } from '@storybook/angular-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-docs'],
+  features: {
+    sidebarOnboardingChecklist: false,
+  },
   framework: {
     name: '@storybook/angular-vite',
     options: {
