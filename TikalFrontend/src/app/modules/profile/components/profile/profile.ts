10ae1541f4c719761bfd0955ab861154a999e6f7
Fabian Mahdi <fabian@mahdi.at>
2026-09-13 15:31:59 +0000

    added empty profile component

---
+++ TikalFrontend/src/app/modules/profile/components/profile/profile.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tikal-profile',
+  templateUrl: './profile.html',
+  styleUrl: './profile.scss',
+})
+export class ProfileComponent {}
