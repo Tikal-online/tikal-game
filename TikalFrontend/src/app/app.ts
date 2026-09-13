e06e66ae392e98644b18ca5bce188abe19ebe170
Fabian Mahdi <fabian@mahdi.at>
2026-09-13 15:02:14 +0000

    sidebar with toggle

--- TikalFrontend/src/app/app.ts
+++ TikalFrontend/src/app/app.ts
@@ -4,11 +4,14 @@ import { AccountStore } from './core/stores/account-store/account-store';
 import { AuthStore } from './core/stores/auth-store/auth-store';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
+import { User } from '@primeicons/angular/user';
+import { Home } from '@primeicons/angular/home';
+import { Sidebar } from '@primeicons/angular/sidebar';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarModule, ButtonModule],
+  imports: [RouterOutlet, SidebarModule, ButtonModule, Home, User, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
