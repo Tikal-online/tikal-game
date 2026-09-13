1445cbed41c4c27df212d2737c764aacb7f0151d
Fabian Mahdi <fabian@mahdi.at>
2026-09-13 16:58:34 +0000

    added route guards

--- TikalFrontend/src/app/app.routes.ts
+++ TikalFrontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './modules/profile/components/profile/profile';
 import { LobbiesComponent } from './modules/lobbies/components/lobbies/lobbies';
+import { isAuthenticated } from './core/route-guards/is-authenticated/is-authenticated-guard';
+import { hasAccount } from './core/route-guards/has-account/has-account-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'Lobbies', pathMatch: 'full' },
-  { path: 'Profile', component: ProfileComponent },
-  { path: 'Lobbies', component: LobbiesComponent },
+  { path: 'Profile', component: ProfileComponent, canActivate: [isAuthenticated, hasAccount] },
+  { path: 'Lobbies', component: LobbiesComponent, canActivate: [isAuthenticated, hasAccount] },
 ];
