aaa4df468131c8e09d3f3fdf65a594e80bb3d687
Fabian Mahdi <fabian@mahdi.at>
2026-09-13 15:40:40 +0000

    added empty lobbies component with routes

--- TikalFrontend/src/app/app.routes.ts
+++ TikalFrontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './modules/profile/components/profile/profile';
+import { LobbiesComponent } from './modules/lobbies/components/lobbies/lobbies';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'Lobbies', pathMatch: 'full' },
+  { path: 'Profile', component: ProfileComponent },
+  { path: 'Lobbies', component: LobbiesComponent },
+];
---
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tikal-lobbies',
+  templateUrl: './lobbies.html',
+  styleUrl: './lobbies.scss',
+})
+export class LobbiesComponent {}
