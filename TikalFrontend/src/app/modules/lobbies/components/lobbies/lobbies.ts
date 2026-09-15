af4b276a3d64e3a309cacd2b31d3eeef2aae385e
Fabian Mahdi <fabian@mahdi.at>
2026-09-15 19:59:05 +0000

    added initial search items

--- TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent, InputComponent } from 'tikal-ui-components';
 
 @Component({
   selector: 'tikal-lobbies',
   templateUrl: './lobbies.html',
   styleUrl: './lobbies.scss',
+  imports: [ButtonComponent, InputComponent],
 })
 export class LobbiesComponent {}
