ffa5ee5ef6a5e3d5bd2fa7c13c4c875bef1580f8
Fabian Mahdi <fabian@mahdi.at>
2026-09-17 20:10:11 +0000

    added initial fetching by searching

--- TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonComponent, InputComponent } from 'tikal-ui-components';
+import { LobbySummaryStore } from '../../stores/lobby/lobby-summary-store';
 
 @Component({
   selector: 'tikal-lobbies',
@@ -7,4 +8,12 @@ import { ButtonComponent, InputComponent } from 'tikal-ui-components';
   styleUrl: './lobbies.scss',
   imports: [ButtonComponent, InputComponent],
 })
-export class LobbiesComponent {}
+export class LobbiesComponent {
+  readonly lobbiesSummaryStore = inject(LobbySummaryStore);
+
+  constructor() {
+    const filter = this.lobbiesSummaryStore.filter;
+
+    this.lobbiesSummaryStore.loadLobbies(filter);
+  }
+}
