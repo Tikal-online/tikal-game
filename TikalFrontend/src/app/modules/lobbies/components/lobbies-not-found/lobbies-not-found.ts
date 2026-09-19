40d25533d3c55bc10b610a67a655d58525b0a4f5
Fabian Mahdi <fabian@mahdi.at>
2026-09-19 18:59:13 +0000

    added translations to lobby not found component

--- TikalFrontend/src/app/modules/lobbies/components/lobbies-not-found/lobbies-not-found.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies-not-found/lobbies-not-found.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
 import { Inbox } from '@primeicons/angular/inbox';
 import { ButtonComponent } from 'tikal-ui-components';
 
 @Component({
   selector: 'tikal-lobbies-not-found',
-  imports: [Inbox, ButtonComponent],
+  imports: [Inbox, ButtonComponent, TranslocoDirective],
   templateUrl: './lobbies-not-found.html',
   styleUrl: './lobbies-not-found.scss',
 })
