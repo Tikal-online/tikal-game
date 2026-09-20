a981c091c8270fb77093a66c5d6aa18a283552d3
Fabian Mahdi <fabian@mahdi.at>
2026-09-20 19:19:26 +0000

    added neverthrow dependency

--- TikalFrontend/src/app/core/components/create-account/create-account.ts
+++ TikalFrontend/src/app/core/components/create-account/create-account.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { disabled, form, maxLength, required } from '@angular/forms/signals';
 import { DialogModule } from 'primeng/dialog';
 import { InputComponent, ButtonComponent } from 'tikal-ui-components';
+import { AccountStore } from '../../stores/account-store/account-store';
+import { ActivatedRoute, Router } from '@angular/router';
+
+type AccountData = {
+  name: string;
+};
 
 @Component({
   selector: 'tikal-create-account',
@@ -8,4 +15,18 @@ import { InputComponent, ButtonComponent } from 'tikal-ui-components';
   templateUrl: './create-account.html',
   styleUrl: './create-account.scss',
 })
-export class CreateAccountComponent {}
+export class CreateAccountComponent {
+  private readonly accountStore = inject(AccountStore);
+
+  private readonly router = inject(Router);
+
+  private readonly route = inject(ActivatedRoute);
+
+  private readonly accountData = signal<AccountData>({ name: '' });
+
+  readonly accountForm = form(this.accountData, (schemaPath) => {
+    required(schemaPath.name);
+    maxLength(schemaPath.name, 30);
+    disabled(schemaPath, { when: () => this.accountForm().submitting() });
+  });
+}
--- TikalFrontend/src/app/core/services/account-service/account-service.ts
+++ TikalFrontend/src/app/core/services/account-service/account-service.ts
@@ -1,9 +1,14 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
+import { err, ok, Result } from 'neverthrow';
 import { catchError, map, Observable, of, throwError } from 'rxjs';
 
 export type Account = {
   userId: string;
+  name: string;
+};
+
+export type AccountExists = {
   name: string;
 };
 
@@ -19,6 +24,21 @@ export class AccountService {
       catchError((error: HttpErrorResponse) => {
         if (error.status === 404) {
           return of(null);
+        }
+
+        return throwError(() => error);
+      }),
+    );
+  }
+
+  createAccount(name: string): Observable<Result<Account, AccountExists>> {
+    const body = { name: name };
+
+    return this.http.post<Account>(this.url, body).pipe(
+      map((account: Account) => ok(account)),
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 409) {
+          return err({ name: name });
         }
 
         return throwError(() => error);
