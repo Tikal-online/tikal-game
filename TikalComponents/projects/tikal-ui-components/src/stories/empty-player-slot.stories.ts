4823ae280552798b313c4066da3f5e5409beb7ce
Fabian Mahdi <fabian@mahdi.at>
2026-09-22 15:23:06 +0000

    minor renaming

--- TikalComponents/projects/tikal-ui-components/src/stories/empty-player-slot.stories.ts
+++ TikalComponents/projects/tikal-ui-components/src/stories/empty-player-slot.stories.ts
@@ -2,7 +2,7 @@ import { applicationConfig, Meta, StoryObj } from '@storybook/angular-vite';
 import { EmptyPlayerSlotComponent } from '../lib/components/empty-player-slot/empty-player-slot';
 
 const meta: Meta<EmptyPlayerSlotComponent> = {
-  title: 'Atoms/EmptyPlayerSlot',
+  title: 'Atoms/Empty-Player-Slot',
   component: EmptyPlayerSlotComponent,
   tags: ['autodocs'],
   decorators: [
