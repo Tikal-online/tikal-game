836ae0c8e62f4c864bddda1f56ae426beaaae28f
Fabian Mahdi <fabian@mahdi.at>
2026-09-22 18:15:09 +0000

    added preview component to route

--- TikalFrontend/src/app/modules/lobbies/components/lobby-preview/lobby-preview.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobby-preview/lobby-preview.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PreviewLobbyStore } from '../../stores/lobby/preview-lobby-store';
 
 @Component({
   selector: 'tikal-lobby-preview',
@@ -6,4 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './lobby-preview.html',
   styleUrl: './lobby-preview.scss',
 })
-export class LobbyPreviewComponent {}
+export class LobbyPreviewComponent {
+  private readonly route = inject(ActivatedRoute);
+
+  readonly previewLobbyStore = inject(PreviewLobbyStore);
+
+  constructor() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.previewLobbyStore.loadLobby(id);
+  }
+}
