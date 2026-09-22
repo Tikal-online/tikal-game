b6f9c6d26ffead966e5167d33d7a50357e36590a
Fabian Mahdi <fabian@mahdi.at>
2026-09-22 17:53:50 +0000

    lobby preview component base

---
+++ TikalFrontend/src/app/modules/lobbies/components/lobby-preview/lobby-preview.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tikal-lobby-preview',
+  imports: [],
+  templateUrl: './lobby-preview.html',
+  styleUrl: './lobby-preview.scss',
+})
+export class LobbyPreviewComponent {}
