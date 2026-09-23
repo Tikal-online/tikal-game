88a0a9d930ea4a0f7daf1f13a246914a7144c510
Fabian Mahdi <fabian@mahdi.at>
2026-09-23 07:49:23 +0000

    added translations

--- TikalFrontend/src/app/modules/lobbies/components/lobby-preview/lobby-preview.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobby-preview/lobby-preview.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PreviewLobbyStore } from '../../stores/lobby/preview-lobby-store';
 import { PlayerSlotListComponent } from 'tikal-ui-components';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'tikal-lobby-preview',
-  imports: [PlayerSlotListComponent],
+  imports: [PlayerSlotListComponent, TranslocoDirective],
   templateUrl: './lobby-preview.html',
   styleUrl: './lobby-preview.scss',
 })
