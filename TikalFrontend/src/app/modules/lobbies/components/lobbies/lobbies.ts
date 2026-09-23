02b8793f0dca86f6770c05f2b4a9edf7c26dc15d
Fabian Mahdi <fabian@mahdi.at>
2026-09-23 05:40:44 +0000

    added preview routing

--- TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
+++ TikalFrontend/src/app/modules/lobbies/components/lobbies/lobbies.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SkeletonComponent, PlayerCountComponent, ChipComponent } from 'tikal-ui-components';
 import { LobbySummaryStore } from '../../stores/lobby/lobby-summary-store';
-import { TableLazyLoadEvent, TableModule } from 'primeng/table';
+import { TableLazyLoadEvent, TableModule, TableRowSelectEvent } from 'primeng/table';
 import { LobbiesHeaderComponent } from '../lobbies-header/lobbies-header';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { LobbiesNotFoundComponent } from '../lobbies-not-found/lobbies-not-found';
+import { Router, RouterLink } from '@angular/router';
+import { LobbySummary } from '../../services/lobby/lobby-service';
 
 @Component({
   selector: 'tikal-lobbies',
@@ -18,9 +20,12 @@ import { LobbiesNotFoundComponent } from '../lobbies-not-found/lobbies-not-found
     LobbiesHeaderComponent,
     TranslocoDirective,
     LobbiesNotFoundComponent,
+    RouterLink,
   ],
 })
 export class LobbiesComponent {
+  private readonly router = inject(Router);
+
   readonly lobbiesSummaryStore = inject(LobbySummaryStore);
 
   constructor() {
